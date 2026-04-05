54c5a22bb2e7068aaa8bb3a554f31dc309acae41
ssccoouutt <144871295+ssccoouutt@users.noreply.github.com>
2026-04-05 16:35:49 +0500

    Replace "https://github.com/chelaxian/tg-ytdlp-bot/wiki/YT_DLP#supported-sites'" with "https://drive.google.com/file/d/1Z42fkMhJwvOjzV5MS2CeHIv_Ch8pA0Oz/view?usp=drivesdk" in down_and_audio.py

--- DOWN_AND_UP/down_and_audio.py
+++ DOWN_AND_UP/down_and_audio.py
@@ -1318,7 +1318,7 @@ def down_and_audio(app, message, url, tags, quality_key=None, playlist_name=None
                     
                     send_error_to_user(
                         message,
-                        "<blockquote>Check <a href='https://github.com/chelaxian/tg-ytdlp-bot/wiki/YT_DLP#supported-sites'>here</a> if your site supported</blockquote>\n"
+                        "<blockquote>Check <a href='https://drive.google.com/file/d/1Z42fkMhJwvOjzV5MS2CeHIv_Ch8pA0Oz/view?usp=drivesdk>here</a> if your site supported</blockquote>\n"
                         "<blockquote>You may need <code>cookie</code> for downloading this audio. First, clean your workspace via <b>/clean</b> command</blockquote>\n"
                         "<blockquote>For Youtube - get <code>cookie</code> via <b>/cookie</b> command. For any other supported site - send your own cookie (<a href='https://t.me/tg_ytdlp/203'>guide1</a>) (<a href='https://t.me/tg_ytdlp/214'>guide2</a>) and after that send your audio link again.</blockquote>\n"
                         f"────────────────\n"
